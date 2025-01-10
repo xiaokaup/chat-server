6fc753e9ad638b8150e2659fef6fc61d1fe108af
Jiean Yang <jiean.yang.externe@emeria.eu>
2025-01-10 11:46:43 +0100

    Create src/routes/authRoutes.ts

--- user-service/src/routes/authRoutes.ts
+++ user-service/src/routes/authRoutes.ts
@@ -3,7 +3,9 @@ import AuthController from "../controllers/AuthController";
 
 const userRouter = Router();
 
+// @ts-ignore
 userRouter.post("/register", AuthController.register);
+// @ts-ignore
 userRouter.post("/login", AuthController.login);
 
 export default userRouter;
